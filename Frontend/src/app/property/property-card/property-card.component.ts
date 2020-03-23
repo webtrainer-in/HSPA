8d05f1d04792a16bf0072aa7bd677d835fff91f5
webtrainer-in <webtrainer-in@gmail.com>
2020-03-23 16:27:52 +0530

    Add Property List
    Signed-off-by: webtrainer-in <webtrainer-in@gmail.com>

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -11,12 +11,6 @@ import { Component } from '@angular/core';
 
 )
 export class PropertyCardComponent {
-
-  Property: any = {
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-  }
+@Input() property : any
 
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Erose Villa",
+      "Type":"Villa",
+      "Price":14000
+    },
+    {
+      "Id":3,
+      "Name":"Mark Hill",
+      "Type":"Flat",
+      "Price":12500
+    },
+    {
+      "Id":4,
+      "Name":"Gun Hill",
+      "Type":"Duplex",
+      "Price":11400
+    },
+    {
+      "Id":5,
+      "Name":"Macro Home",
+      "Type":"House",
+      "Price":14300
+    },
+    {
+      "Id":6,
+      "Name":"Pearl White House",
+      "Type":"House",
+      "Price":18000
+    }
+]
+
   constructor() { }
 
   ngOnInit(): void {
