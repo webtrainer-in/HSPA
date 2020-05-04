16f949316d6d578e671830a66aacaafa47609dfc
webtrainer-in <webtrainer-in@gmail.com>
2020-05-04 15:15:34 +0530

    Adding Template Driven Form
    Signed-off-by: webtrainer-in <webtrainer-in@gmail.com>

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {Routes, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -31,6 +32,7 @@ const appRoutes: Routes = [
    imports: [
       BrowserModule,
       HttpClientModule,
+      FormsModule,
       RouterModule.forRoot(appRoutes)
    ],
    providers: [
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
@@ -7,15 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropertyForm: NgForm;
+  SellRent = '1';
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  onBack()
-  {
+  onBack() {
     this.router.navigate(['/']);
+  }
+
+  onSubmit(Form: NgForm) {
+    console.log('Congrats, form Submitted');
+    console.log(this.addPropertyForm);
   }
 
 }
