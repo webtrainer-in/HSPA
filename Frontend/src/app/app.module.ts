8b0f2efe26cb2ec98ce23cb55e3003dae885ea86
Sandeep <webtrainer.in@gmail.com>
2020-06-21 20:03:38 +0530

    Add new property part-2
    Signed-off-by: Sandeep <webtrainer.in@gmail.com>

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -51,7 +52,8 @@ const appRoutes: Routes = [
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
-      ButtonsModule.forRoot()
+      ButtonsModule.forRoot(),
+      BsDatepickerModule.forRoot()
    ],
    providers: [
      HousingService,
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -11,16 +12,23 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm: NgForm;
   @ViewChild('formTabs') formTabs: TabsetComponent;
-  SellRent = '1';
+
+  // Will come from masters
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex']
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished']
+
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
+
   constructor(private router: Router) { }
 
   ngOnInit() {
-    // this.addPropertyForm.controls['Name'].setValue('Default Value');
-
-    setTimeout(() => {
-      this.addPropertyForm.controls['Name'].setValue('Default Value');
-    });
-
   }
 
   onBack() {
