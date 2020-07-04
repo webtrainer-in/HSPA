dde590125a40f76058de9387c42139f55d11d543
Sandeep <webtrainer.in@gmail.com>
2020-07-04 17:02:45 +0530

    Add validation for all fields
    Signed-off-by: Sandeep <webtrainer.in@gmail.com>

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
 import { IPropertyBase } from 'src/app/model/ipropertybase';
 
@@ -33,7 +33,6 @@ export class AddPropertyComponent implements OnInit {
     RTM: null
   };
 
-
   constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
@@ -43,35 +42,146 @@ export class AddPropertyComponent implements OnInit {
   CreateAddPropertyForm() {
     this.addPropertyForm = this.fb.group({
       BasicInfo: this.fb.group({
-        SellRent: [null, Validators.required],
+        SellRent: ['1' , Validators.required],
+        BHK: [null, Validators.required],
         PType: [null, Validators.required],
-        Name: [null, Validators.required]
+        FType: [null, Validators.required],
+        Name: [null, Validators.required],
+        City: [null, Validators.required]
       }),
+
       PriceInfo: this.fb.group({
         Price: [null, Validators.required],
-        BuiltArea: [null, Validators.required]
-    })
-    });
+        BuiltArea: [null, Validators.required],
+        CarpetArea: [null],
+        Security: [null],
+        Maintenance: [null],
+      }),
+
+      AddressInfo: this.fb.group({
+        FloorNo: [null],
+        TotalFloor: [null],
+        Address: [null, Validators.required],
+        LandMark: [null],
+      }),
+
+      OtherInfo: this.fb.group({
+        RTM: [null, Validators.required],
+        PosessionOn: [null],
+        AOP: [null],
+        Gated: [null],
+        MainEntrance: [null],
+        Description: [null]
+      })
+      });
   }
 
-  // -------------------
-  // Getter Methods
-  // -------------------
-  get BasicInfo() {
-    return this.addPropertyForm.controls.BasicInfo as FormGroup;
-  }
+//#region <Getter Methods>
+  // #region <FormGroups>
+      get BasicInfo() {
+        return this.addPropertyForm.controls.BasicInfo as FormGroup;
+      }
 
-  get PriceInfo() {
-    return this.addPropertyForm.controls.PriceInfo as FormGroup;
-  }
+      get PriceInfo() {
+        return this.addPropertyForm.controls.PriceInfo as FormGroup;
+      }
 
-  get SellRent() {
-    return this.BasicInfo.controls.SellRent as FormControl;
-  }
+      get AddressInfo() {
+        return this.addPropertyForm.controls.AddressInfo as FormGroup;
+      }
 
-  get Price() {
-    return this.PriceInfo.controls.Price as FormControl;
-  }
+      get OtherInfo() {
+        return this.addPropertyForm.controls.OtherInfo as FormGroup;
+      }
+  // #endregion
+
+  //#region <Form Controls>
+      get SellRent() {
+        return this.BasicInfo.controls.SellRent as FormControl;
+      }
+
+      get BHK() {
+        return this.BasicInfo.controls.BHK as FormControl;
+      }
+
+      get PType() {
+        return this.BasicInfo.controls.PType as FormControl;
+      }
+
+      get FType() {
+        return this.BasicInfo.controls.FType as FormControl;
+      }
+
+      get Name() {
+        return this.BasicInfo.controls.Name as FormControl;
+      }
+
+      get City() {
+        return this.BasicInfo.controls.City as FormControl;
+      }
+
+      get Price() {
+        return this.PriceInfo.controls.Price as FormControl;
+      }
+
+      get BuiltArea() {
+        return this.PriceInfo.controls.BuiltArea as FormControl;
+      }
+
+      get CarpetArea() {
+        return this.PriceInfo.controls.CarpetArea as FormControl;
+      }
+
+      get Security() {
+        return this.PriceInfo.controls.Security as FormControl;
+      }
+
+      get Maintenance() {
+        return this.PriceInfo.controls.Maintenance as FormControl;
+      }
+
+      get FloorNo() {
+        return this.AddressInfo.controls.FloorNo as FormControl;
+      }
+
+      get TotalFloor() {
+        return this.AddressInfo.controls.TotalFloor as FormControl;
+      }
+
+      get Address() {
+        return this.AddressInfo.controls.Address as FormControl;
+      }
+
+      get LandMark() {
+        return this.AddressInfo.controls.LandMark as FormControl;
+      }
+
+      get RTM() {
+        return this.OtherInfo.controls.RTM as FormControl;
+      }
+
+      get PossesioOn() {
+        return this.OtherInfo.controls.PosessionOn as FormControl;
+      }
+
+      get AOP() {
+        return this.OtherInfo.controls.AOP as FormControl;
+      }
+
+      get Gated() {
+        return this.OtherInfo.controls.Gated as FormControl;
+      }
+
+      get MainEntrace() {
+        return this.OtherInfo.controls.MainEntrance as FormControl;
+      }
+
+      get Description() {
+        return this.OtherInfo.controls.Description as FormControl;
+      }
+
+  //#endregion
+//#endregion
 
   onBack() {
     this.router.navigate(['/']);
@@ -88,8 +198,18 @@ export class AddPropertyComponent implements OnInit {
       this.formTabs.tabs[1].active = true;
       return;
     }
-    console.log('Congrats, form Submitted');
-    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
+
+    if (this.AddressInfo.invalid) {
+      this.formTabs.tabs[2].active = true;
+      return;
+    }
+
+    if (this.OtherInfo.invalid) {
+      this.formTabs.tabs[3].active = true;
+      return;
+    }
+
+    console.log('Congrats, your property listed succeffully on our website');
     console.log(this.addPropertyForm);
   }
 
