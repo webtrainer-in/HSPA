756ca8ecf35e3d4ef3c84b1e8da429cf3e23b4de
Sandeep <webtrainer.in@gmail.com>
2021-03-03 04:55:48 +0530

    Migrate to Angular 11
    Signed-off-by: Sandeep <webtrainer.in@gmail.com>

--- Frontend/src/app/nav-bar/nav-bar.component.spec.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('NavBarComponent', () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NavBarComponent ]
     })
